6db6720a982acee80265214b0af1aae4dc4a13ee
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-08-07 16:43:34 +0700

    recovery

---
+++ out/artifacts/case_study_03_war_exploded/error.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: stpdm
+  Date: 8/7/2020
+  Time: 4:25 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Error</title>
+</head>
+<body>
+Error
+</body>
+</html>
---
+++ src/service/clothing/IClothingService.java
@@ -0,0 +1,32 @@
+package service.clothing;
+
+import model.Clothing;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IClothingService {
+
+    public List<Clothing> findAll();
+
+    public List<Clothing> findAllClothingCategory();
+
+    public void insert(Clothing clothing) throws SQLException;
+
+
+    public boolean update(Clothing clothing) throws SQLException;
+
+    public boolean remove(int id) throws SQLException;
+
+    public List<Clothing> findByPrice(int price) throws SQLException;
+
+    public List<Clothing> findByStatus(String status) throws SQLException;
+
+    public List<Clothing> findAllByStatus(String status);
+
+    public List<String> findAllCategoryStatus();
+
+    //List<Clothing> findByCategoryID(int id, String status) throws SQLException;
+
+    public List<Clothing> findByCategoryID(int id) throws SQLException;
+}
