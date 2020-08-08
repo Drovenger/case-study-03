0a94111e51a9e72099b5590199ad8e7c1b7aeadb
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-08-08 14:44:35 +0700

    repair connection

--- src/main/java/util/DBHandle.java
+++ src/main/java/util/DBHandle.java
@@ -9,15 +9,17 @@ public class DBHandle {
     }
 
     public static Connection getConnection() {
-        String urlDB = "jdbc:mysql://localhost:3306/duongshoe";
-        String userDB = "root";
-        String passDB = "123456";
-
+        Connection connection = null;
         try {
-            return DriverManager.getConnection(urlDB, userDB, passDB);
-        } catch (SQLException e) {
+            Class.forName("com.mysql.jdbc.Driver");
+            String jdbcURL = "jdbc:mysql://localhost:3306/online_shop?useSSL=false";
+            String jdbcUsername = "root";
+            String jdbcPassword = "Mc11041992$";
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException | ClassNotFoundException e) {
+            //TODO Auto-generated catch block
             e.printStackTrace();
         }
-        return null;
+        return connection;
     }
 }
